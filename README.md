da8914e252ecaf1e33f74958a380da84fb0191bb
younotafish <cunjisong@tju.edu.cn>
2020-07-28 12:08:40 -0700

    fist commit and change from inmemory db to mongodb

---
+++ README.md
@@ -0,0 +1,30 @@
+# Sample Application
+
+The main aim of developing this application is to explore the working of nodejs and expressjs.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+Below steps are needed only for the first time when you setup your dev machine
+
+Install node.js server from https://nodejs.org/en/download/
+
+### Installing
+
+1. Clone this git repository, if you haven't done already.
+
+   `git clone https://github.com/cmevawala/nodejs-todo-app.git`
+
+2. Install local npm dependencies
+
+   `npm install`
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Built With
+
+* [Node](https://nodejs.org/en/) - JavaScript runtime built on Chrome's V8 JavaScript engine
+* [Express](https://expressjs.com/) - Node.js web application framework
---
+++ controllers/todocontroller.js
@@ -0,0 +1,68 @@
+var bodyparser = require("body-parser");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://admin:admin@ds135532.mlab.com:35532/todolist");
+
+// Create Schema
+var todoschema = new mongoose.Schema({
+  item: String,
+});
+
+// Create Model
+var TodoModel = mongoose.model("Todo", todoschema);
+
+// Save Item in Model
+// var firstItem = TodoModel({ item: "Sachin Tendular"}).save(function(error) {
+//   if(error) throw error;
+//
+//   console.log("Item Saved");
+// })
+
+var urlencodedparser = bodyparser.urlencoded({ extended: false }); // ??
+
+module.exports = function (app) {
+  app.get("/todo", function (request, response) {
+    // Get todos from MongoDB
+    TodoModel.find({}, function (error, data) {
+      if (error) throw error;
+
+      response.render("todo", { todos: data });
+    });
+
+    // Im Memory
+    // response.render("todo", {todos: data});
+  });
+
+  app.post("/todo", urlencodedparser, function (request, response) {
+    // Insert todo into MongoDB
+    var newTodo = TodoModel(request.body).save(function (error, data) {
+      if (error) throw error;
+
+      response.json(data);
+      // Variants
+      //     response.send() // ?
+      //     response.render() // ?
+    });
+
+    // In Memory
+    // data.push(request.body)
+    // response.json(data);
+  });
+
+  app.delete("/todo/:item", function (request, response) {
+    // Delete todo from MongoDB
+    TodoModel.find({ item: request.params.item.replace(/\-/g, " ") }).remove(
+      function (error, data) {
+        if (error) throw error;
+
+        response.json(data);
+      }
+    );
+
+    // In Memory
+    // data = data.filter(function(todo) {
+    //   return todo.item.replace(/ /g, '-') !== request.params.item;
+    // });
+    //
+    // response.json(data); // ?
+  });
+};
